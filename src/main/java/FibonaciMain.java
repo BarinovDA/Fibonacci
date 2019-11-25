bb30622b2c48bb891bba51932857057388503ae0
BarinovDA <52528663+BarinovDA@users.noreply.github.com>
2019-11-25 13:49:09 +0300

    Initial commit

---
+++ src/main/java/FibonaciMain.java
@@ -0,0 +1,37 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FibonaciMain {
+    private static Map<Integer, BigInteger> mapa = new HashMap<Integer, BigInteger>();
+
+    public static void main(String[] args) {
+    }
+
+    public static BigInteger fibonacciSequence (int n){
+        if (n == 0 || n == 1) return BigInteger.ONE;
+        if (mapa.containsKey(n)) return mapa.get(n);
+        BigInteger v = fibonacciSequence(n-2).add(fibonacciSequence(n-1));
+        mapa.put(n, v);
+        return v;
+    }
+
+    public static int calculateZero (BigInteger n){
+        int zeroCount = 0;
+        BigInteger num = n;
+        while (num.compareTo(BigInteger.ZERO) > 0) {
+            if (num.remainder(BigInteger.TEN).compareTo(BigInteger.ZERO) == 0) {
+                zeroCount++;
+                num = num.divide(BigInteger.TEN);
+            } else {
+                num = num.divide(BigInteger.TEN);
+            }
+        }
+        return zeroCount;
+    }
+
+    public static int zeroInFibonacci (int n){
+       BigInteger fibonacci = fibonacciSequence(n);
+        return calculateZero(fibonacci);
+    }
+}
